ff121c42e85c628f83f6cc82e72ed2d75d6a048b
Sahil-Prasad22 <prasadsahil1234@gmail.com>
2026-05-03 18:13:02 +0530

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,67 @@
+import React, { use } from 'react'
+import { Box } from 'lucide-react'
+import Button from './ui/Button'
+import { useOutletContext } from 'react-router'
+
+
+const Navbar = () => {
+  const { isSignedIn,userName,signIn,signOut } = useOutletContext<AuthContext>()
+
+
+    const hanldeAuthClick = async() => {
+            if(isSignedIn){
+                try{
+                    await signOut();
+                }catch(e){
+                    console.error(`Puter sign out failed: ${e}`)
+        }}
+        try{            
+            await signIn();
+        }catch(e){
+            console.error(`Puter sign in failed: ${e}`);
+        }
+    };
+
+    return (
+        <header className ='navbar'>
+            <nav className ='inner'>
+                <div className='left'>
+                    <div className ='brand'>
+                        <Box className = "logo"/>
+                        <span className='name'>Roomify</span>
+                    </div>
+                    <ul className='links'>
+                        <a href="#">Product</a>
+                        <a href="#">Pricing</a>
+                        <a href="#">Comunity</a>
+                        <a href="#">Enterprise</a>
+
+                    </ul>
+                </div>
+                <div className='actions'>
+                    
+                    {isSignedIn ? (
+                            <>
+                                <span className='username'>
+                                    {userName ? `Hi, ${userName }`: 'Signed In'}
+                                </span>
+                                <Button size='sm' onClick={hanldeAuthClick} className='btn'>
+                                    Log out
+                                    </Button>
+                            </> 
+                    ) : (
+                    <>
+                        <Button  onClick={hanldeAuthClick} size="sm" variant='ghost'>
+                            Log In
+                        </Button>
+                    <a href="#upload" className='cta'> Get Started</a>
+                    </>
+                )}
+                    
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
