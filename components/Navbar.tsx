73b20ce38d1d183d76bd8402f0c67704c83139fc
Sahil-Prasad22 <prasadsahil1234@gmail.com>
2026-05-15 11:59:59 +0530

    file updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,63 +1,68 @@
-import React, { use } from 'react'
-import { Box } from 'lucide-react'
-import Button from './ui/Button'
-import { useOutletContext } from 'react-router'
-
+import {Box} from "lucide-react";
+import Button from "./ui/Button";
+import {useOutletContext} from "react-router";
 
 const Navbar = () => {
-  const { isSignedIn,userName,signIn,signOut } = useOutletContext<AuthContext>()
+    const { isSignedIn, userName, signIn, signOut } = useOutletContext<AuthContext>()
 
+    const handleAuthClick = async () => {
+        if(isSignedIn) {
+            try {
+                await signOut();
+            } catch (e) {
+                console.error(`Puter sign out failed: ${e}`);
+            }
 
-    const hanldeAuthClick = async() => {
-            if(isSignedIn){
-                try{
-                    await signOut();
-                }catch(e){
-                    console.error(`Puter sign out failed: ${e}`)
-        }}
-        try{            
+            return;
+        }
+
+        try {
             await signIn();
-        }catch(e){
+        } catch (e) {
             console.error(`Puter sign in failed: ${e}`);
         }
     };
 
     return (
-        <header className ='navbar'>
-            <nav className ='inner'>
-                <div className='left'>
-                    <div className ='brand'>
-                        <Box className = "logo"/>
-                        <span className='name'>Roomify</span>
+        <header className="navbar">
+            <nav className="inner">
+                <div className="left">
+                    <div className="brand">
+                        <Box  className="logo" />
+
+                        <span className="name">
+                            Roomify
+                        </span>
                     </div>
-                    <ul className='links'>
+
+                    <ul className="links">
                         <a href="#">Product</a>
                         <a href="#">Pricing</a>
-                        <a href="#">Comunity</a>
+                        <a href="#">Community</a>
                         <a href="#">Enterprise</a>
-
                     </ul>
                 </div>
-                <div className='actions'>
-                    
+
+                <div className="actions">
                     {isSignedIn ? (
-                            <>
-                                <span className='username'>
-                                    {userName ? `Hi, ${userName }`: 'Signed In'}
-                                </span>
-                                <Button size='sm' onClick={hanldeAuthClick} className='btn'>
-                                    Log out
-                                    </Button>
-                            </> 
+                        <>
+                            <span className="greeting">
+                                {userName ? `Hi, ${userName}` : 'Signed in'}
+                            </span>
+
+                            <Button size="sm" onClick={handleAuthClick} className="btn">
+                                Log Out
+                            </Button>
+                        </>
                     ) : (
-                    <>
-                        <Button  onClick={hanldeAuthClick} size="sm" variant='ghost'>
-                            Log In
-                        </Button>
-                    <a href="#upload" className='cta'> Get Started</a>
-                    </>
-                )}
-                    
+                        <>
+                            <Button onClick={handleAuthClick} size="sm" variant="ghost">
+                                Log In
+                            </Button>
+
+                            <a href="#upload" className="cta">Get Started</a>
+                        </>
+                    )}
                 </div>
             </nav>
         </header>
